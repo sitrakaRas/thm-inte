e2b4cc3c7e44eee950fbee5e7bc68478eb971108
sitraka <sitraka@bocasay.fr>
2016-11-02 18:11:55 +0300

    section partenaire

--- html/app/scripts/main.js
+++ html/app/scripts/main.js
@@ -1,5 +1,40 @@
+var thinkmarket = {
+	slider: function(parent,arg){
+		parent.slick(arg);
+	}
+}
+
 $(function(){
 	//same height block
 	$(".row-shiffter .block-shifter").matchHeight();
 	$(".logo-wrapper .block-logo").matchHeight();
+
+	//slider partenaire
+	var arg = {
+		slidesToShow : 4,
+		dots : true,
+		infinite: true,
+		responsive : [
+			{
+				breakpoint : 1024,
+				settings: {
+			        slidesToShow: 3
+			    }
+			},
+			{
+				breakpoint : 768,
+				settings: {
+			        slidesToShow: 2
+			    }
+			},
+			{
+				breakpoint : 480,
+				settings: {
+			        slidesToShow: 1
+			    }
+			}
+		]
+
+	};
+	thinkmarket.slider($(".slider-part"),arg);
 });
