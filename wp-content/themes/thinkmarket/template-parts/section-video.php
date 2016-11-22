a7d80039dc1f397a9b32d2aa632cd8141b8b1a13
sitraka <sitraka@bocasay.fr>
2016-11-22 17:33:31 +0300

    slider home dynamique

--- wp-content/themes/thinkmarket/template-parts/section-video.php
+++ wp-content/themes/thinkmarket/template-parts/section-video.php
@@ -1,51 +1,35 @@
+<?php wp_reset_query(); ?>
 <section id="slidervideo">
-    <div class="slidervideoctnr">
-    <!-- items -->
-      <div class="items">
-        <div class="col-md-12">
-          <div class="row">
-            <video class="video-play" preload="none" loop autoplay>
-              <source src="<?php blogInfo("template_url"); ?>/video/CamilleRio.mp4" type="video/mp4" />
-            </video>  
-            <div class="content col-md-4 col-md-offset-7">
-              <h3><a href="#">join the shift</a></h3>
-              <p>En 4 ans je suis intervenu dans de multiples secteurs pour des clients de haut niveau.</p>
-              <div class="auteur">
-                <p><strong>Mélanie Dupont</strong></p>
-                <span>Manager</span>
-              </div>
-            </div>         
-          </div>
-         
+  <div class="slidervideoctnr">
+  <?php if(have_rows("slider_video")): ?>
+  <?php   while(have_rows("slider_video")): the_row();?>
+  <!-- items -->
+    <div class="items">
+      <div class="col-md-12">
+        <div class="row">
+          <video class="video-play" preload="none" loop autoplay>
+            <source src="<?php echo get_sub_field("fichier_video"); ?>" type="video/mp4" />
+          </video>  
+          <div class="content col-md-4 col-md-offset-7">
+            <h3><a href="<?php echo get_field("lien_vers_nous_rejoindre"); ?>">join the shift</a></h3>
+            <p><?php echo get_sub_field("resume") ?></p>
+            <div class="auteur">
+              <p><strong><?php echo get_sub_field("nom_et_prenom"); ?></strong></p>
+              <span><?php get_sub_field("poste_personne"); ?></span>
+            </div>
+          </div>         
         </div>
-      </div>
-    <!-- ./items -->
-    <!-- items -->
-      <div class="items">
-        <div class="col-md-12">
-          <div class="row">
-            <video class="video-play" preload="none" loop autoplay>
-              <source src="<?php blogInfo("template_url"); ?>/video/CelineForestier.mp4" type="video/mp4" />
-            </video>  
-            <div class="content col-md-4 col-md-offset-7">
-              <h3><a href="#">join the shift</a></h3>
-              <p>En 4 ans je suis intervenu dans de multiples secteurs pour des clients de haut niveau.</p>
-              <div class="auteur">
-                <p><strong>Mélanie Dupont</strong></p>
-                <span>Manager</span>
-              </div>
-            </div>         
-          </div>
-         
-        </div>
-      </div>
-    <!-- ./items -->
-    </div>
-    <!-- bottom-link -->
-    <div class="col-md-12 bottom-link bleu">
-      <div class="link-ctnr">
-        <a href="#">Nous rejoindre</a>
+       
       </div>
     </div>
-    <!-- ./bottom-link -->
-  </section>
+  <!-- ./items -->
+  <?php endwhile;endif; ?>
+  </div>
+  <!-- bottom-link -->
+  <div class="col-md-12 bottom-link bleu">
+    <div class="link-ctnr">
+      <a href="<?php echo get_field("lien_vers_nous_rejoindre"); ?>">Nous rejoindre</a>
+    </div>
+  </div>
+  <!-- ./bottom-link -->
+</section>
