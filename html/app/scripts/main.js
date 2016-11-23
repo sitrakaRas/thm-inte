3c88ee04a58506cf58c90499282365666f947af1
idealmg <rocky@ideal.mg>
2016-11-23 15:36:42 +0300

    Intégration page "Clients"

--- html/app/scripts/main.js
+++ html/app/scripts/main.js
@@ -1,219 +1,242 @@
 var thinkmarket = {
-	slider: function(parent,arg){
-		parent.slick(arg);
-	},
-	headfixe : function(parent,arg){
-		parent.scrollToFixed(arg);
-	},
-	resizeSlide : function(elem,arg){
-		var sectionSize = $(window).height();
-		if(arg.sub){
-			sectionSize = sectionSize - arg.sub;
-		}
-		elem.height(sectionSize);
-	},
-	equalHeight : function(container){
-		var currentTallest = 0,
-		     currentRowStart = 0,
-		     rowDivs = new Array(),
-		     $el,
-		     topPosition = 0,
-		     currentDiv;
+    slider: function(parent,arg){
+        parent.slick(arg);
+    },
+    headfixe : function(parent,arg){
+        parent.scrollToFixed(arg);
+    },
+    resizeSlide : function(elem,arg){
+        var sectionSize = $(window).height();
+        if(arg.sub){
+            sectionSize = sectionSize - arg.sub;
+        }
+        elem.height(sectionSize);
+    },
+    equalHeight : function(container){
+        var currentTallest = 0,
+            currentRowStart = 0,
+            rowDivs = new Array(),
+            $el,
+            topPosition = 0,
+            currentDiv;
 
-		 $(container).each(function() {
+        $(container).each(function() {
 
-		   $el = $(this);
-		   topPosition = $el.position().top;
-		   
-		   if (currentRowStart != topPosition) {
+            $el = $(this);
+            topPosition = $el.position().top;
 
-		     // we just came to a new row.  Set all the heights on the completed row
-		     for (currentDiv = 0 ; currentDiv < rowDivs.length ; currentDiv++) {
-		       rowDivs[currentDiv].height(currentTallest);
-		     }
+            if (currentRowStart != topPosition) {
 
-		     // set the variables for the new row
-		     rowDivs.length = 0; // empty the array
-		     currentRowStart = topPosition;
-		     currentTallest = $el.height();
-		     rowDivs.push($el);
+                // we just came to a new row.  Set all the heights on the completed row
+                for (currentDiv = 0 ; currentDiv < rowDivs.length ; currentDiv++) {
+                    rowDivs[currentDiv].height(currentTallest);
+                }
 
-		   } else {
+                // set the variables for the new row
+                rowDivs.length = 0; // empty the array
+                currentRowStart = topPosition;
+                currentTallest = $el.height();
+                rowDivs.push($el);
 
-		     // another div on the current row.  Add it to the list and check if it's taller
-		     rowDivs.push($el);
-		     currentTallest = (currentTallest < $el.height()) ? ($el.height()) : (currentTallest);
+            } else {
 
-		  }
-		   
-		  // do the last row
+                // another div on the current row.  Add it to the list and check if it's taller
+                rowDivs.push($el);
+                currentTallest = (currentTallest < $el.height()) ? ($el.height()) : (currentTallest);
 
-		   for (currentDiv = 0 ; currentDiv < rowDivs.length ; currentDiv++) {
-		     rowDivs[currentDiv].height(currentTallest);
-		   }
-		   
-		})
-	}
+            }
+
+            // do the last row
+
+            for (currentDiv = 0 ; currentDiv < rowDivs.length ; currentDiv++) {
+                rowDivs[currentDiv].height(currentTallest);
+            }
+
+        })
+    }
 }
 
 $(function(){
-	//same height block
-	$(".row-shiffter .block-shifter").matchHeight();
-	$(".logo-wrapper .block-logo").matchHeight();
+    //same height block
+    $(".row-shiffter .block-shifter").matchHeight();
+    $(".logo-wrapper .block-logo").matchHeight();
 
-	//slider partenaire
-	var arg = {
-		slidesToShow : 4,
-		dots : true,
-		infinite: true,
-		easing: 'easeOut',
-		responsive : [
-			{
-				breakpoint : 1024,
-				settings: {
-			        slidesToShow: 3
-			    }
-			},
-			{
-				breakpoint : 768,
-				settings: {
-			        slidesToShow: 2
-			    }
-			},
-			{
-				breakpoint : 480,
-				settings: {
-			        slidesToShow: 1
-			    }
-			}
-		]
+    //slider partenaire
+    var arg = {
+        slidesToShow : 4,
+        dots : true,
+        infinite: true,
+        easing: 'easeOut',
+        responsive : [
+            {
+                breakpoint : 1024,
+                settings: {
+                    slidesToShow: 3
+                }
+            },
+            {
+                breakpoint : 768,
+                settings: {
+                    slidesToShow: 2
+                }
+            },
+            {
+                breakpoint : 480,
+                settings: {
+                    slidesToShow: 1
+                }
+            }
+        ]
 
-	};
-	thinkmarket.slider($(".slider-part"),arg);
-	
-	//menu fixe
-	var menu_arg;
-	thinkmarket.headfixe($(".main-nav"),menu_arg);
+    };
+    thinkmarket.slider($(".slider-part"),arg);
 
-	$(window).on("scroll",function(){
-		if($(".main-nav").offset().top > 10 ){
-			$(".main-nav").addClass("minimize");
-		}else{
-			$(".main-nav").removeClass("minimize");
-		}
+    //slider citation
+    var arg_cit = {
+        dots: true
+    }
+    thinkmarket.slider($("#citation.slider"),arg_cit);
 
-	});
+    //menu fixe
+    var menu_arg;
+    thinkmarket.headfixe($(".main-nav"),menu_arg);
 
-	//top slider
-	var arg_St = {
-		dots : true,
-		easing : 'easeOut',
-		infinite: true,
-		adaptiveHeight: true
-	};
+    $(window).on("scroll",function(){
+        if($(".main-nav").offset().top > 10 ){
+            $(".main-nav").addClass("minimize");
+        }else{
+            $(".main-nav").removeClass("minimize");
+        }
 
-	thinkmarket.slider($(".slider-top"),arg_St);
+    });
 
-	// business case 
-	var arg_bc = {
-		dots : true,
-		easing : 'easeOut',
-		infinite: true,
-		fade: true
-	};
-	thinkmarket.slider($(".slider-bc"),arg_bc);
+    //top slider
+    var arg_St = {
+        dots : true,
+        easing : 'easeOut',
+        infinite: true,
+        adaptiveHeight: true
+    };
 
-	//slider-schifter
-	var arg_sh = {
-		dots : true,
-		slidesToScroll: 3,
-		slidesToShow : 3,
-		easing: 'easeOut'
-	}
+    thinkmarket.slider($(".slider-top"),arg_St);
 
-	thinkmarket.slider($(".slider-schifter"),arg_sh);
+    // business case 
+    var arg_bc = {
+        dots : true,
+        easing : 'easeOut',
+        infinite: true,
+        fade: true
+    };
+    thinkmarket.slider($(".slider-bc"),arg_bc);
 
-	//slider-expertise
-	var arg_ex = {
-		dots : true,
-		slidesToScroll: 1,
-		slidesToShow : 3,
-		easing: 'easeOut',
-		responsive : [
-			{
-				breakpoint : 1024,
-				settings: {
-			        slidesToShow: 2
-			    }
-			},
-			{
-				breakpoint : 768,
-				settings: {
-			        slidesToShow: 1
-			    }
-			}
-		]
-	}
+    //slider-schifter
+    var arg_sh = {
+        dots : true,
+        slidesToScroll: 3,
+        slidesToShow : 3,
+        easing: 'easeOut',
+        responsive: [
+            {
+                breakpoint: 991,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2,
+                }
+            },
+            {
+                breakpoint: 767,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                }
+            }
+        ]
 
-	thinkmarket.slider($(".slider-expertise"),arg_ex);
-	
-	$(window).on("resize", function(){
-		var arg_slide = {
-			sub : 90
-		};
+    }
 
-		thinkmarket.resizeSlide($(".item-top"),arg_slide);
-		
-		// Init Skrollr
-		var s = skrollr.init();
-		// Refresh Skrollr after resizing our sections
-		s.refresh($('#block-top'));
+    thinkmarket.slider($(".slider-schifter"),arg_sh);
+
+    //slider-expertise
+    var arg_ex = {
+        dots : true,
+        slidesToScroll: 1,
+        slidesToShow : 3,
+        easing: 'easeOut',
+        responsive : [
+            {
+                breakpoint : 1024,
+                settings: {
+                    slidesToShow: 2
+                }
+            },
+            {
+                breakpoint : 768,
+                settings: {
+                    slidesToShow: 1
+                }
+            }
+        ]
+    }
+
+    thinkmarket.slider($(".slider-expertise"),arg_ex);
+
+    $(window).on("resize", function(){
+        var arg_slide = {
+            sub : 90
+        };
+
+        thinkmarket.resizeSlide($(".item-top"),arg_slide);
+
+        // Init Skrollr
+        var s = skrollr.init();
+        // Refresh Skrollr after resizing our sections
+        s.refresh($('#block-top'));
 
 
-	 	//same height #recute
+        //same height #recute
 
-	 	if($("#recrute").length > 0 ){
-	 		thinkmarket.equalHeight(".processus-wrapper .block");
-	 	}
-	});
+        if($("#recrute").length > 0 ){
+            thinkmarket.equalHeight(".processus-wrapper .block");
+        }
+    });
 
-	$(window).trigger("resize");
+    $(window).trigger("resize");
 
-	var arg_vid = {
-		dots : true,
-		easing: 'easeOut',
-		lazyLoad: 'ondemand'
-	};
- 	thinkmarket.slider($(".slidervideoctnr"),arg_vid);
+    var arg_vid = {
+        dots : true,
+        easing: 'easeOut',
+        lazyLoad: 'ondemand'
+    };
+    thinkmarket.slider($(".slidervideoctnr"),arg_vid);
 
- 	//playing video slider
- 	if($("#slidervideo").length > 0){
- 		$("#slidervideo .slick-active .video-play")[0].play();
- 	}
+    //playing video slider
+    if($("#slidervideo").length > 0){
+        $("#slidervideo .slick-active .video-play")[0].play();
+    }
 
- 	$(".slidervideoctnr").on("afterChange",function(event,slick){
- 		$("#slidervideo .slick-active .video-play")[0].play();
- 	});
- 	$(".slidervideoctnr").on("beforeChange",function(event,slick){
- 		$("#slidervideo .items .video-play")[0].pause();
- 	});
+    $(".slidervideoctnr").on("afterChange",function(event,slick){
+        $("#slidervideo .slick-active .video-play")[0].play();
+    });
+    $(".slidervideoctnr").on("beforeChange",function(event,slick){
+        $("#slidervideo .items .video-play")[0].pause();
+    });
 
- 	//slider top scroll to
- 	$("#block-top .to-next-btn a").on("click",function(e){
- 		e.preventDefault();
- 		var id = "#" + $("#block-top").next().attr("id");
- 		$("html,body").animate({scrollTop: $(id).offset().top},500);
- 	});
+    //slider top scroll to
+    $("#block-top .to-next-btn a").on("click",function(e){
+        e.preventDefault();
+        var id = "#" + $("#block-top").next().attr("id");
+        $("html,body").animate({scrollTop: $(id).offset().top},500);
+    });
 
- 	//add class on hover
- 	if($("#joins_us").length > 0){
- 		$("#joins_us a").on("mouseover",function(){
- 				$("#joins_us").toggleClass("hover");
- 		});
- 		$("#joins_us a").on("mouseout",function(){
- 				$("#joins_us").toggleClass("hover");
- 		});
- 	}
- 	
+    //add class on hover
+    if($("#joins_us").length > 0){
+        $("#joins_us a").on("mouseover",function(){
+            $("#joins_us").toggleClass("hover");
+        });
+        $("#joins_us a").on("mouseout",function(){
+            $("#joins_us").toggleClass("hover");
+        });
+    }
+
 
 });
