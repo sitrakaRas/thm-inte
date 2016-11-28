7cca012ef59bb556190a4d682020b51cd58bb672
sitraka <sitraka@bocasay.fr>
2016-11-28 09:55:29 +0300

    twitter bloc page actu

--- wp-content/themes/thinkmarket/template/template-actualite.php
+++ wp-content/themes/thinkmarket/template/template-actualite.php
@@ -69,16 +69,18 @@ global $post;
                 	else :
 					?>
 					<!-- twitter block -->
-	                <div class="col-md-4">
+	                <div class="col-md-4 bltw">
 	                    <div class="twitter-block">
-	                        <h3><a href="https://twitter.com/<?php the_field('id_twitter', 'option'); ?>">nos tweets</a></h3>
+	                        <h3><a href="https://twitter.com/<?php the_field('id_twitter', 'option'); ?>" target="_blank">nos tweets</a></h3>
 	                        <div class="listing">
 	                           <a class="twitter-timeline" href="https://twitter.com/<?php the_field('id_twitter', 'option'); ?>" data-show-replies="false"  data-aria-polite="assertive"data-chrome="nofooter noborders noheader transparent" data-tweet-limit="5"><?php the_field('id_twitter', 'option'); ?></a> <script async src="//platform.twitter.com/widgets.js" charset="utf-8"></script>
 	                        </div>
-	                        <div class="link-more">
-	                            <a href="#">tweet</a>
-	                        </div>
-	                    </div>             
+	                    </div>     
+						<div class="bottom-link">
+	                     	<div class="link-more">
+	                            <a href="https://twitter.com/<?php the_field('id_twitter', 'option'); ?>" target="_blank">tweet</a>
+	                        </div> 
+	                    </div>   
 	                </div>
 	                <!-- ./twitter block -->
 	                <div class="col-md-4 all <?php echo $categ[0]->slug; ?>">
