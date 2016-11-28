d93c2704d8b32ce4748bf51fd33e31b73f8ef371
idealmg <rocky@ideal.mg>
2016-11-28 18:21:58 +0300

    Traitement des retours (suite) + Support dev: • problèmes cellule 1 Home - it's time. • home cellule 1 animation rollover shift • header menu textuel • home cellule 1 effet parallax • home cellule 2 manque rollover picto • a propos cellule 1 ancre chevron • supports divers au developpeur

--- html/app/scripts/main.js
+++ html/app/scripts/main.js
@@ -61,6 +61,15 @@ var thinkmarket = {
 }
 
 $(function(){
+
+    //browser detection
+    var bDetection = browserDetection();
+    var bName = bDetection.browser;
+    var bVers = bDetection.version;
+    var bOs = bDetection.os;
+    $("body").addClass(bName + " " + bName + "-" + bVers + " " + bOs);
+
+
     //same height block
     $(".row-shiffter .block-shifter").matchHeight();
     $(".logo-wrapper .block-logo").matchHeight();
@@ -99,27 +108,27 @@ $(function(){
     var arg_cit = {
         dots: true
     }
-    thinkmarket.slider($("#citation .slider"),arg_cit);
-	
-	//menu fixe
-	var menu_arg;
-	thinkmarket.headfixe($(".main-nav"),menu_arg);
+    thinkmarket.slider($("#citation.slider"),arg_cit);
 
-	$(window).on("scroll",function(){
-		if($(".main-nav").offset().top > 10 ){
-			$(".main-nav").addClass("minimize");
-		}else{
-			$(".main-nav").removeClass("minimize");
-		}
+    //menu fixe
+    var menu_arg;
+    thinkmarket.headfixe($(".main-nav"),menu_arg);
 
-	});
+    $(window).on("scroll",function(){
+        if($(".main-nav").offset().top > 10 ){
+            $(".main-nav").addClass("minimize");
+        }else{
+            $(".main-nav").removeClass("minimize");
+        }
 
-	//top slider
-	var arg_St = {
-		dots : true,
-		easing : 'easeOut',
-		infinite: true,
-		adaptiveHeight: true,
+    });
+
+    //top slider
+    var arg_St = {
+        dots : true,
+        easing : 'easeOut',
+        infinite: true,
+        adaptiveHeight: true,
         responsive : [
             {
                 breakpoint : 768,
@@ -132,6 +141,23 @@ $(function(){
     };
 
     thinkmarket.slider($(".slider-top"),arg_St);
+
+    /*if($('body').length){ //bug safari
+        var $safariBody = $('body');
+        var $sliderTop = $(".slider-top");
+        getSafariBgImage($sliderTop);
+
+        function getSafariBgImage($sliderTop){
+            var bgImage = $sliderTop.find(".slick-active").css("background-image");
+            console.log(bgImage);
+            $safariBody.css({'backgroundImage':bgImage});
+        }
+
+        $sliderTop.on('afterChange', function(event, slick, direction){
+            getSafariBgImage($sliderTop);
+            // left
+        });
+    }*/
 
     // business case 
     var arg_bc = {
@@ -207,6 +233,25 @@ $(function(){
 
     thinkmarket.slider($(".slider-expertise"),arg_ex);
 
+
+    //parralaxe slider
+    (function(){
+        var parallax = document.querySelectorAll("#block-top .item-top"),
+            speed = 0.5;
+
+        $(window).scroll(function(){
+            [].slice.call(parallax).forEach(function(el,i){
+
+                var windowYOffset = $(window).scrollTop(),
+                    elBackgrounPos = "center " + (windowYOffset * speed) + "px";
+
+                el.style.backgroundPosition = elBackgrounPos;
+
+            });
+        });
+
+    })();
+
     $(window).on("resize", function(){
         var arg_slide = {
             sub : 90
@@ -215,9 +260,11 @@ $(function(){
         thinkmarket.resizeSlide($(".item-top"),arg_slide);
 
         // Init Skrollr
+        /*--28.11.2016
         var s = skrollr.init();
         // Refresh Skrollr after resizing our sections
         s.refresh($('#block-top'));
+        */
 
 
         //same height #recute
@@ -308,12 +355,40 @@ $(function(){
         $(this).parent().toggleClass("active");
     });
 
-    //browser detection
-    var bDetection = browserDetection();
-    var bName = bDetection.browser;
-    var bVers = bDetection.version;
-    var bOs = bDetection.os;
-    $("body").addClass(bName + " " + bName + "-" + bVers + " " + bOs);
+    //patch javascript
+
+    (function _0037156_0037171() { //correction retournement shift
+        var $h1 = $("#block-top h1");
+        if($h1.text()=="It's time to shift.It's time to shift."){
+            $h1.html('<h1>It\'s time<br>to <a class="shift" href="#" tabindex="0"><span>shift</span></a>.</h1>');
+        }
+
+        var $shift = $h1.find(".shift");
+        $shift.on('mouseenter mouseout',function(){
+            if(!$shift.hasClass('spinning')){
+                $shift.addClass('spinning')
+                setTimeout(function(){$shift.removeClass('spinning')},300)
+                $shift.toggleClass('spin');
+            }
+        });
+    })();
+
+
+    (function _0037177() { //ajout rollover image bleu
+        var $img = $("#shifter-part a.link-offre img");
+        $img.each(function(){
+            var $imgClone =  $(this).clone();
+            var src = $imgClone.attr("src");
+            var regexp = /(.*)(\.[^.]*)$/;
+            var output = regexp.exec(src);
+            $imgClone.attr("src",output[1]+"-hover"+output[2]);
+            $(this).after($imgClone);
+        });
+    })();
+
+
+    /*(function _0037156() {
+    })()*/
 
 
 });
